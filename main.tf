019ddba75c4219ca37442fbf2a1714341e89f182
naresh240 <naresh240.qis@gmail.com>
2026-04-27 10:07:49 +0530

    calling module in terraform private registry

--- main.tf
+++ main.tf
@@ -2,7 +2,7 @@ module "ec2-instance" {
   source  = "app.terraform.io/aws9am_devops/ec2-instance/aws"
   version = "1.0.0"
   
-  inst_name             = "Helloworld"
+  inst_name             = "ec2-instance"
   inst_type             = "t3.micro"
   key_name              = "aws8pm"
   security_group_names  = ["default"]
